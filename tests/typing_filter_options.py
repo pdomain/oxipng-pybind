f76e483e74964cbd397fb88ea69e1d66993d5c90
ConcaveTrillion <concavetrillion@gmail.com>
2026-05-27 21:20:22 +0000

    test: align typing fixture with iterable filters

--- tests/typing_filter_options.py
+++ tests/typing_filter_options.py
@@ -6,17 +6,18 @@ png_data = b"not used by basedpyright"
 
 optimize_from_memory(png_data, filter=FilterStrategy.predefined(["none", "sub"]))
 optimize_from_memory(png_data, filter=[FilterStrategy.none, "sub"])
-optimize_from_memory(
-    png_data,
-    filter={FilterStrategy.none, FilterStrategy.sub},  # pyright: ignore[reportArgumentType]
-)
-optimize_from_memory(
-    png_data,
-    filter=[FilterStrategy.predefined(["none", "sub"])],  # pyright: ignore[reportArgumentType]
-)
+optimize_from_memory(png_data, filter={FilterStrategy.none, FilterStrategy.sub})
+optimize_from_memory(png_data, filter=[FilterStrategy.predefined(["none", "sub"])])
 
 RawImage(1, 1, ColorType.indexed, BitDepth.eight, b"\x00", palette=[(255, 0, 0)])
-RawImage(1, 1, ColorType.indexed, BitDepth.eight, b"\x00", palette=[[255, 0, 0]])
+RawImage(
+    1,
+    1,
+    ColorType.indexed,
+    BitDepth.eight,
+    b"\x00",
+    palette=[[255, 0, 0]],  # pyright: ignore[reportArgumentType]
+)
 RawImage(
     1,
     1,
